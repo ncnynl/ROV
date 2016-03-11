8ee5986a825c8351f9429f864f58e9da3102411e
falafel72 <ksh717@student.cis.edu.hk>
2016-03-11 11:40:40 +0800

    Initial commit for socket.io functionality

--- App/app.js
+++ App/app.js
@@ -8,7 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,5 +59,15 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//socket.io connection
+io.on('connection',function(socket){
+  socket.on('motor',function(values){
+
+  });
+});
+
+
+//to start the server
+http.listen(8080);
 
 module.exports = app;
