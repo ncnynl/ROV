00e308fcdb564dc15dff10981fc5300331aa071d
Arthelon <hsing.daniel@gmail.com>
2016-03-12 17:01:55 +0800

    Fixed SerialPort bug

--- App/app.js
+++ App/app.js
@@ -12,23 +12,24 @@ var users = require('./routes/users');
 var app = express();
 var server = app.listen(8080);
 var io = require("socket.io")(server);
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
 
 //serialport setup
 var portname = ''; //needs port name
-var serialport = require('serialport');
-var port = new SerialPort(portname,{
-  baudRate: 9600,
-  parser: serialport.parsers.readline('\n')
+var port = new SerialPort(portname, {
+    baudRate: 9600,
+    parser: serialport.parsers.readline('\n')
 });
 
 var temp;
 //assigns incoming temperature data to temp variable
 port.on('data', (data)=> {
-  temp = data;
+    temp = data;
 });
 
 port.on('error', (err)=> {
-  console.log("Error: " + err);
+    console.log("Error: " + err);
 })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,7 +39,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -46,10 +47,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -57,35 +58,35 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 //socket.io connection
-io.on('connection',function(socket){
-  //send motor values to arduino
-  socket.on('onChange',function(values){
-    port.write(`${values['vertical_f']} ${values['vertical_b']} ${values['left_m']} ${values['right_m']} ${values['arm']}`);
-  });
-  //continuously send sensor data to client
-  setInterval(()=>{
-    io.emit('sensorData',temp); 
-  },1000);
+io.on('connection', function (socket) {
+    //send motor values to arduino
+    socket.on('onChange', function (values) {
+        port.write(`${values['vertical_f']} ${values['vertical_b']} ${values['left_m']} ${values['right_m']} ${values['arm']}`);
+    });
+    //continuously send sensor data to client
+    setInterval(()=> {
+        io.emit('sensorData', temp);
+    }, 1000);
 });
 
 
