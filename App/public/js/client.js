1b6f2e7284e0b5222b5374f8fb6e92d2bdfe3f52
Arthelon <hsing.daniel@gmail.com>
2016-03-12 17:25:41 +0800

    Added input with sliders

--- App/public/js/client.js
+++ App/public/js/client.js
@@ -1,17 +1,18 @@
 //Joystick / Keyboard
 //0-1023
-//{
-//    left_m:
-//    right_m:
-//    vertical_f:
-//    vertical_b:
-//    arm:
-//}
-// Events: onChange, sensorData
-// continually receive sensor data from server
+//
 $(function() {
-    var socket = io.connect('http://localhost')
+    var socket = io.connect('http://localhost') //Enter valid network IP
     socket.on('sensorData', function(temp) {
-
+        $('.tempData').val(temp)
+    })
+    $('#dataInp').keypress(function(evt) {
+        if (evt.which == 13) { //Enter Key
+            payload = {}
+            $(this).children().each(function() {
+                payload[$(this).attr('id')] = $(this).val()
+            })
+            socket.emit('onChange', payload)
+        }
     })
 });
