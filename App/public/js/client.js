b797e8a0a620ae247c02c1f61a96c8ef6b869ad1
oswinso <oswinso@gmail.com>
2016-03-17 13:08:57 +0800

    Moved motor value sending from webcam.js to client.js

--- App/public/js/client.js
+++ App/public/js/client.js
@@ -1,14 +1,37 @@
 //Joystick / Keyboard
 //0-1023
 //
-$(function() {
-    var socket = io.connect('http://localhost') //Enter valid network IP
-    socket.on('sensorData', function(temp) {
-        $('.tempData').val(temp)
-    })
-    // socket.on('start-stream', function() {
-    //     wsavc.playStream()
-    // })
+
+var motorValues = {
+    vertical_f: 0,
+    left_m: 0,
+    right_m: 0,
+    vertical_b: 0,
+    arm: 0
+}
+
+var tmpValues = {
+    vertical_f: 0,
+    left_m: 0,
+    right_m: 0,
+    vertical_b: 0,
+    arm: 0
+}
+
+(function($) {
+    $(document).ready(function() {
+        var socket = io.connect('http://localhost:3000') //Enter valid network IP
+        socket.on('connect', function() {
+            window.setInterval(motorController, 10);
+        });
+        socket.on('sensorData', function(temp) {
+            $('.tempData').val(temp)
+        })
+
+    });
+})(jQuery);​
+
+
     $('#dataInp').keypress(function(evt) {
         if (evt.which == 13) { //Enter Key
             payload = {}
@@ -18,4 +41,57 @@ $(function() {
             socket.emit('onChange', payload)
         }
     });
-});
+};  
+
+function motorController() {
+    
+    var changed = false
+    var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+    if (!gamepads) {
+        return;
+    }
+    var gp = gamepads[0];
+    var tmpValues.left_m = (gp.axes[1] * 100).toFixed(2)*-1;
+    var tmpValues.right_m = (gp.axes[3] * 100).toFixed(2)*-1;
+
+    //Front Motors
+    if(buttonPressed(gp.buttons[4])) {
+        tmpValues.vertical_f += 1;
+    }
+    if(buttonPressed(gp.buttons[6])) {
+        tmpValues.vertical_f -= 1;
+    }
+    //Back Motors
+    if(buttonPressed(gp.buttons[5])) {
+        tmpValues.vertical_b += 1;
+    }
+    if(buttonPressed(gp.buttons[7])) {
+        tmpValues.vertical_b -= 1;
+    }
+    $.each(tmpValues, function(key, value) {
+        if(value > 100) {
+            tmpValues[key] = 100;
+        }
+        if(value < -100) {
+            tmpValues[key] = -100;
+        }
+        if(value != motorValues[key]) {
+            changed = true;
+            motorValues[key] = value;
+        }
+    })
+    if(changed) {
+        socket.emit('onChange', motorValues);
+    }
+    $("vertical_f").text(motorValues.vertical_f);
+    $("left_m").text(motorValues.left_m);
+    $("right_m").text(motorValues.right_m);
+    $("vertical_b").text(motorValues.vertical_b);
+}
+
+function buttonPressed(b) {
+    if(typeof(b) == "object") {
+        return b.pressed;
+    }
+    return b == 1.0;
+}
